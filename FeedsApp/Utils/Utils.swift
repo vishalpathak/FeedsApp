430abda9c66410ca5e53c7b1893728a7e7f1c7dc
VishalP <vishalp@gmail.com>
2020-06-23 00:06:56 +0530

    Convert UI to requirement -- Convert likes and comments in Short number format -- convert time to time ago format

--- FeedsApp/Utils/Utils.swift
+++ FeedsApp/Utils/Utils.swift
@@ -7,3 +7,83 @@
 //
 
 import Foundation
+
+class Util {
+    
+    static func getElapsedInterval(fromDate: Date) -> String {
+
+        let interval = Calendar.current.dateComponents([.year, .month, .day], from: fromDate, to: Date())
+
+        if let year = interval.year, year > 0 {
+            return year == 1 ? "\(year)" + " " + "year ago" :
+                "\(year)" + " " + "years ago"
+        } else if let month = interval.month, month > 0 {
+            return month == 1 ? "\(month)" + " " + "month ago" :
+                "\(month)" + " " + "months ago"
+        } else if let day = interval.day, day > 0 {
+            return day == 1 ? "\(day)" + " " + "day ago" :
+                "\(day)" + " " + "days ago"
+        } else {
+            return "a moment ago"
+        }
+
+    }
+    
+    static func convertDateFormat(stringDate: String, givenFormat: String, expectedFormat: String) -> String?
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = givenFormat
+        let date = dateFormatter.date(from: stringDate)
+        dateFormatter.dateFormat = expectedFormat
+        if let dateNew = date {
+            return  dateFormatter.string(from: dateNew)
+        } else{
+            return nil
+        }
+    }
+    
+    static func convertStringToDate(stringDate: String, givenFormat: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = givenFormat
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.locale = Locale.current
+        return dateFormatter.date(from: stringDate)
+    }
+}
+
+extension Int {
+
+    func shortNumbersFormat () -> String {
+        let numFormatter = NumberFormatter()
+
+        typealias Abbrevation = (threshold:Double, divisor:Double, suffix:String)
+        let abbreviations:[Abbrevation] = [(0, 1, ""),
+                                           (1000.0, 1000.0, "K"),
+                                           (100_000.0, 1_000_000.0, "M"),
+                                           (100_000_000.0, 1_000_000_000.0, "B")]
+                    
+
+        let startValue = Double (abs(self))
+        let abbreviation:Abbrevation = {
+            var prevAbbreviation = abbreviations[0]
+            for tmpAbbreviation in abbreviations {
+                if (startValue < tmpAbbreviation.threshold) {
+                    break
+                }
+                prevAbbreviation = tmpAbbreviation
+            }
+            return prevAbbreviation
+        } ()
+
+        let value = Double(self) / abbreviation.divisor
+        numFormatter.positiveSuffix = abbreviation.suffix
+        numFormatter.negativeSuffix = abbreviation.suffix
+        numFormatter.allowsFloats = true
+        numFormatter.minimumIntegerDigits = 1
+        numFormatter.minimumFractionDigits = 0
+        numFormatter.maximumFractionDigits = 1
+
+        return numFormatter.string(from: NSNumber (value:value))!
+    }
+
+}
--- FeedsApp/ViewModel/FeedsViewModel.swift
+++ FeedsApp/ViewModel/FeedsViewModel.swift
@@ -12,8 +12,8 @@ struct FeedsInfoViewModel {
     let id : String
     let createdAt : String
     let content : String
-    let comments : Int32
-    let likes : Int32
+    let comments : String
+    let likes : String
     let avatar: String
     let image: String
     let articleUrl: String
@@ -22,7 +22,7 @@ struct FeedsInfoViewModel {
     let lastName: String
     
     init(id: String, created: String, content: String,
-         comments: Int32, likes: Int32, avatar: String,
+         comments: String, likes: String, avatar: String,
          image: String, articleUrl: String, name: String, designation: String, lastName: String) {
         self.id = id
         self.createdAt = created
@@ -35,5 +35,76 @@ struct FeedsInfoViewModel {
         self.name = name
         self.lastName = lastName
         self.designation = designation
+    }
+}
+
+protocol DataReceivedDelegate: class {
+    func didGetDataFromAPI(rowViewModel: [FeedsInfoViewModel]?, error: String?)
+}
+
+protocol MapFeedDataModel {
+    func mapToViewModelFromAPI(feeds: [Feeds]) -> [FeedsInfoViewModel]
+}
+
+class FeedDataInfoVM: MapFeedDataModel {
+    
+    weak var dataReceivedDelegate: DataReceivedDelegate?
+    
+    var arrViewModel = [FeedsInfoViewModel]()
+    
+    func fetchDataFromApi(page: Int, limit: Int){
+        let networkManager = NetworkManager()
+        networkManager.getNewFeeds(page: page, limit: limit) { (feedss, error) in
+            if let error = error{
+                self.dataReceivedDelegate?.didGetDataFromAPI(rowViewModel: nil, error: error)
+                return
+            }
+            
+            if let feed = feedss{
+                if feed.count > 0{
+                    let arr = self.mapToViewModelFromAPI(feeds: feed)
+                    self.dataReceivedDelegate?.didGetDataFromAPI(rowViewModel: arr, error: nil)
+                }
+            }
+        }
+    }
+    
+    func mapToViewModelFromAPI(feeds: [Feeds]) -> [FeedsInfoViewModel] {
+        
+        for i in 0..<feeds.count{
+            let obj = feeds[i]
+            let dt = Util.convertStringToDate(stringDate: obj.createdAt ?? "", givenFormat: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            var created: String = ""
+            if let dt = dt{
+                created = Util.getElapsedInterval(fromDate: dt)
+            }
+            let likes = obj.likes?.shortNumbersFormat()
+            let comment = obj.comments?.shortNumbersFormat()
+            if let user = obj.user, let media = obj.media{
+                if user.count > 0 && media.count > 0{
+                    let objUser = user[0]
+                    let objMedia = media[0]
+                    
+                    let objView = FeedsInfoViewModel(id: obj.id ?? "NoId", created: created ,
+                                                     content: obj.content ?? "NoContent", comments: comment ?? "",
+                                                     likes: likes ?? "", avatar: objUser.avatar ?? "NoAvatar",
+                                                     image: objMedia.image ?? "NoImage", articleUrl: objMedia.url ?? "NoUrl",
+                                                     name: objUser.name ?? "", designation: objUser.designation ?? "",
+                                                     lastName: objUser.lastname ?? "")
+                    arrViewModel.append(objView)
+                }
+                else if user.count > 0{
+                    let objUser = user[0]
+                    let objView = FeedsInfoViewModel(id: obj.id ?? "NoId", created: created,
+                                                     content: obj.content ?? "NoContent", comments: comment ?? "",
+                                                     likes: likes ?? "", avatar: objUser.avatar ?? "NoAvatar",
+                                                     image: "" , articleUrl: "" ,
+                                                     name: objUser.name ?? "", designation: objUser.designation ?? "",
+                                                     lastName: objUser.lastname ?? "")
+                    arrViewModel.append(objView)
+                }
+            }
+        }
+        return arrViewModel
     }
 }
